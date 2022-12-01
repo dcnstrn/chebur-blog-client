359dedee4ea254d6568c28ded15a2d4239c2bf9d
deconsctruction.events <sanimw3@gmail.com>
2022-12-01 14:12:53 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,7 @@ export const Home = () => {
           {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => isPostsLoading ? ( <Post key={index} isLoading={true}/> ) :  ( <Post
               id={obj._id}
               title={obj.title}
-              imageUrl={obj.imageUrl ? `http://localhost:4444${obj.imageUrl}` : ''}
+              imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}
               // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
               user={obj.user}
               createdAt={obj.createdAt}
